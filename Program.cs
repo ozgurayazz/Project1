a3cb40c5edbb0f5efaaec8e0fe4d6b76924b4b92
User <User@OZGURPC>
2023-11-29 21:59:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Proje_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Giriniz.");
+            string ad = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz.");
+            string soyad = Console.ReadLine();
+            Console.WriteLine("Yaşınızı Giriniz.");
+            string yas = Console.ReadLine();
+            Console.WriteLine($"Merhaba {ad} {soyad} Yaşınız {yas} dir. ");
+            Console.ReadKey();
+        }
+    }
+}
